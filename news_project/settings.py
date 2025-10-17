864cd8dbdee9aac1c2d11958f9d6c02acb605856
xolane0908 <xolanemathenjwa@icloud.com>
2025-10-17 17:49:06 +0200

    Final project updates: Fix database settings and add capstone submission file

--- news_project/settings.py
+++ news_project/settings.py
@@ -77,18 +77,10 @@ WSGI_APPLICATION = 'news_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {
+DATABASES = DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'news_app_db',
-        'USER': 'news_app_user',
-        'PASSWORD': 'your_secure_password',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
